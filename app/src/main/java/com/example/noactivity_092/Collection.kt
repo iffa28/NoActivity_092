9f5e00d64051fb9fd1d349c7ab9946bb48040145
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-12 16:50:44 +0700

    menambahkan variabel untuk menambahkan, mengubah dan menghapus data dari Map Muatble

--- app/src/main/java/com/example/noactivity_092/Collection.kt
+++ app/src/main/java/com/example/noactivity_092/Collection.kt
@@ -68,7 +68,20 @@ fun ContohMap() {
     val readOnlyShape: Map<String,Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
 
+    //Map Mutable
     val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    //menambahkan data dari Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    //menghapus data dari Map mutable
+    shape.remove("Circle")
+    println(shape)
+
+    //mengubah data di dalam map mutable
+    shape["Square"] = 5
     println(shape)
 }
 
