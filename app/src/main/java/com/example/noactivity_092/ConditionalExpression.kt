2155ec05370fa84f215a6c9ecac40dd6ac0ce1f7
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-12 20:55:00 +0700

    menambahkan perulangan dari setiap dari jenis mobil

--- app/src/main/java/com/example/noactivity_092/ConditionalExpression.kt
+++ app/src/main/java/com/example/noactivity_092/ConditionalExpression.kt
@@ -40,7 +40,10 @@ fun Perulangan() {
     for ( i in 1..5) {
         println("Perulangan ke-$i")
     }
-
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
 
 }
 fun main() {
