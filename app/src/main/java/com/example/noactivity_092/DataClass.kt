0d517318d51eea426a5c15634b80a01eb1597adb
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-12 21:26:22 +0700

    menambahkan fungsi equals dengan membandingkan dengan data sebelumnya yaitu id nya sama

--- app/src/main/java/com/example/noactivity_092/DataClass.kt
+++ app/src/main/java/com/example/noactivity_092/DataClass.kt
@@ -7,4 +7,10 @@ data class DataClass(
  fun main() {
      val data = DataClass(1, "iffa@gmail.com")
      println(data)
+
+     //fungsi equals
+     val data2 = DataClass(1, "zahra@gmail.com")
+     println(data == data2)  //false
+
+
  }
