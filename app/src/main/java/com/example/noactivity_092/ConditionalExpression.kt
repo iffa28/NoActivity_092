e9f88c4b2a61ce25f912bd184d359c09fdd0ce14
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-12 20:15:02 +0700

    menambahkan kondisi

--- app/src/main/java/com/example/noactivity_092/ConditionalExpression.kt
+++ app/src/main/java/com/example/noactivity_092/ConditionalExpression.kt
@@ -2,8 +2,13 @@ package com.example.noactivity_092
 
 fun ConditionalStatement() {
     println("===Conditional Expression ===")
-
-
+    //if-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
 }
 
 fun main() {
