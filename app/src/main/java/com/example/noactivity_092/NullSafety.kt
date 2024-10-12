0b5a64835bceb276cc53238acdfeb41415d2a30f
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-12 22:52:39 +0700

    deklarasi variable boleh null

--- app/src/main/java/com/example/noactivity_092/NullSafety.kt
+++ app/src/main/java/com/example/noactivity_092/NullSafety.kt
@@ -3,6 +3,14 @@ package com.example.noactivity_092
 fun nullSafety() {
     //neverNull has string type
     var neverNull: String = "This can't be null"
+
+    //throws a compiler error
+    //neverNull = null
+
+    var nullable: String? = "You can keep a null here"
+    //this is OK
+    nullable = null
+
 }
 
 fun main() {
